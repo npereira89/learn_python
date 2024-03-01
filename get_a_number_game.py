910660f2bfce7a3a773028c3661ab094a0fc9966
nuno-m-pereira-alb <nuno-m-pereira@alticelabs.com>
2024-03-01 16:20:18 +0000

    Fix sonarqube error and warning

--- get_a_number_game.py
+++ get_a_number_game.py
@@ -5,7 +5,8 @@ import random
 import os
 
 def sorted_number():
-    numbers = random.randrange(1, 100)
+    #numbers = random.randrange(1, 100)
+    numbers= random.randint(1,100)
     return numbers
 
 def check_if_is_got(get_number, get_number_sorted):
@@ -24,7 +25,6 @@ for attemps in range(2, -1, -1):
     check_if_is_got(get_number, get_number_sorted)
     
     if get_number != get_number_sorted and attemps !=0:
-        #attemps -=1
         print(f"You have {attemps} attemps")
     elif get_number == get_number_sorted:
         print("You get it!! You're the master!😊")
