5fa49c4072853ad00c6a8c22f469c5e1ce6c180b
Nuno Pereira <82775049+npereira89@users.noreply.github.com>
2024-09-25 18:39:46 +0100

    fixing sync files
    - operation copy and remove
    - operation time to loop folder sync

--- sync_folders.py
+++ sync_folders.py
@@ -3,13 +3,15 @@
 #########################################
 import os
 import hashlib
+import time
 
 
-def remove_files(dire, checkdir):
-    for files_in_dir in os.listdir(checkdir):
+def remove_files(dire, check_dir):
+    for files_in_dir in os.listdir(check_dir):
         file_to_remove = os.path.join(dire, files_in_dir)
         if not os.path.exists(file_to_remove):
-            os.remove(os.path.join(checkdir, files_in_dir))
+            os.remove(os.path.join(check_dir, files_in_dir))
+            print(f"Removing... {os.path.join(check_dir, files_in_dir)}")
 
 
 def calculate_md5(data):
@@ -27,18 +29,25 @@ def calculate_md5(data):
 def list_all_files(dire, replica):
     files, list_files = [], []
     if len(os.listdir(dire)) != 0:
+        remove_files(dire, replica)
         for content in os.listdir(dire):
             path_folder = os.path.join(dire, content)
+            replica_folder = os.path.join(replica, content)
             if os.path.isdir(path_folder):
-                list_all_files(path_folder, replica + "\\" + content)
+                if len(os.listdir(path_folder)) != 0 and not os.path.exists(replica_folder):
+                    os.mkdir(replica_folder)
+                list_all_files(path_folder, replica_folder)
             elif os.path.isfile(path_folder):
-                remove_files(dire, replica)
                 files += [path_folder]
                 replica_folder = os.path.join(replica, content)
                 if (not os.path.exists(replica_folder)
                         or calculate_md5(path_folder) != calculate_md5(replica_folder)):
-                    list_files.append(files[-1:])
-        return list_files
+                    os.system(f"copy {path_folder} {replica} | clip")
+                    print(f"Copying... {path_folder}")
+        return files[-1:]
 
 
-print(list_all_files("folder_src", "folder_dst"))
+while True:
+    print("Beginning...\n")
+    list_all_files("src", "dst")
+    time.sleep(3 * 60)
