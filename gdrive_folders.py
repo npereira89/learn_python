f721568fb0387e0c8d3bac15a57647b352fe2f9b
Nuno Pereira <82775049+npereira89@users.noreply.github.com>
2025-01-15 11:31:58 +0000

    Update gdrive_folders.py
    - testing with folder with 49 files

--- gdrive_folders.py
+++ gdrive_folders.py
@@ -13,11 +13,11 @@ gauth.LocalWebserverAuth()
 drive = GoogleDrive(gauth)
 now = datetime.now()
 
-file_list = drive.ListFile({'q': " title='Fotos'"}).GetList()
+file_list = drive.ListFile({'q': " title='folder_name'"}).GetList()
 tmp_dir = os.path.join(os.getcwd(), "gdrive_folder")
 
 for file1 in file_list:
     link = link + file1['id']
     download_folder(url=link, output=tmp_dir, skip_download=False, use_cookies=False)
-
+    
 shutil.make_archive(f"zip_file_{now.strftime('%d%m%Y_%H%M%S')}", 'zip', tmp_dir)
