9e38b57c082a3c24f86bcd8c6d1937fa7d18693c
Nuno Pereira <82775049+npereira89@users.noreply.github.com>
2025-01-16 17:05:34 +0000

    Fixing script
    - If the path is a directory the program get a error. Fix it!

--- getting_photos_directory.py
+++ getting_photos_directory.py
@@ -1,21 +1,22 @@
 from datetime import datetime
 import code
 import os.path
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 
 now = datetime.now()
 
-print(now.strftime("%d/%m/%Y, %H:%M:%S"))
-print(now.strftime("%d %B of %Y"))
-
-images = []
-path = "C://<your directory>"
-valid_files = [".jpg", ".gif", ".png"]
+path = "C://Users//Nuno//Pictures//"
+valid_files = [".jpg", ".gif", ".png", ".jpeg", ".webp"]
 
 for f in os.listdir(path):
     ext = os.path.splitext(f)[1]
-    if ext.lower() not in valid_files:
-        continue
-    image_path = os.path.join(path, f)
-    with Image.open(image_path) as img:
-        img.show()
+    if ext.lower() in valid_files:
+        image_path = os.path.join(path, f)
+        if os.path.isfile(image_path):
+            try:
+                img_data = Image.open(image_path)
+                img_data.show()
+            except UnidentifiedImageError:
+                print(f"The path {image_path} is a directory")
+            except Exception as e:
+                print(f"An unexpected error occurred: {e}")
