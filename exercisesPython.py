2694f9b7b8364c4d64205fa6618720a4160d31bf
Nuno Pereira <82775049+npereira89@users.noreply.github.com>
2025-02-05 19:00:48 +0000

    calc subsequence and next value
    calc subsequence and next value

--- exercisesPython.py
+++ exercisesPython.py
@@ -858,6 +858,19 @@ john.is_eligible()
 ##################################
 
 ##################################
+## Write a program which asks the user to type in a limit. The program then calculates 
+## the sum of consecutive numbers (1 + 2 + 3 + ...) until the sum is at least equal to the limit set by the user. 
+###################################
+
+limit_value = int(input("Limit: "))
+total, x = 0, 0
+
+while total < limit_value:
+    x += 1
+    total += x
+print(total)
+
+##################################
 ## Write a program which asks the user to type in an upper limit. 
 ## The program then prints out numbers so that each subsequent number is the previous one doubled, 
 ## starting from the number 1. That is, the program prints out powers of two in order.
