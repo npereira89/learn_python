4ddda9a111d8d4e9fd7acebcbc7c35a5fc118fdf
Nuno Pereira <nunosp15@hotmail.com>
2025-02-18 18:05:42 +0000

    save and load data excel

--- application.py
+++ application.py
@@ -1,16 +1,10 @@
 import tkinter as tk
 from tkinter import ttk
-from tkinter import messagebox
 from openpyxl import load_workbook
-
-# Function to print the text
-def print_text():
-    name = text_widget.get("1.0", "end-1c")
-    if label_name == '':
-        messagebox.showwarning(title="Greeting", message="You must input your name!!", option='OK')
-    else:
-        label_name_greet = tk.Label(root, text=f"Hello, {name}")
-        label_name_greet.pack(padx=1, pady=1)
+from openpyxl.styles import Border, Side
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from colorama import Fore, Style
 
 def load_excel_data():
     global tree, root
@@ -18,7 +12,7 @@ def load_excel_data():
     sheet = workbook.active
 
     # Create a Treeview widget
-    tree = ttk.Treeview(root)
+    tree = ttk.Treeview(windows)
     tree.pack(expand=True, fill="both")
 
     # # Get column names from the first row
@@ -36,32 +30,62 @@ def load_excel_data():
     for row in list(sheet.iter_rows(values_only=True))[2:]:
         tree.insert("", 2, values=row)
 
+def save_excel_info():
+    wb = load_workbook("formatted_table.xlsx")
+    sheet_ranges = wb.active
+
+    now = datetime.now()
+    new_date = now + relativedelta(months=3)
+    value_invest = int(input("Value invest: "))
+
+    data = [
+        [now.strftime("%d/%m/%Y"), int(value_invest), new_date.strftime("%d/%m/%Y"), 0.0275, "FALSE"]
+    ]
+
+    for row in range(2, len(data) + 2):
+        max_row = sheet_ranges.max_row + 1
+        for col in range(1, len(data[0]) + 1):
+            sheet_ranges.cell(row=max_row, column=col).value = data[row - 2][col - 1]
+            cell = sheet_ranges.cell(row=max_row, column=col)
+
+            # Formatting borders data
+            cell.border = Border(top=Side(style='thin'), bottom=Side(style='thin'), left=Side(style='thin'),
+                                 right=Side(style='thin'))
+
+            # Formatting cells
+            if col == 2 and cell.row == max_row:
+                cell.number_format = '#,##0€'
+            elif col == 4 and cell.row == max_row:  # Format tax applied
+                cell.value = round(cell.value * 100, 2)
+
+    print(Style.BRIGHT + "The invest was added with success!")
+
+    wb.save("formatted_table.xlsx")
+
+    print(Style.BRIGHT + Fore.GREEN + "Document saved!")
+    wb.close()
+    print(Style.BRIGHT + Fore.WHITE + "Document close!")
+
 # Create the main window
-root = tk.Tk()
-root.title("AforroApp")
-root.iconbitmap("python_icon.ico")
+windows = tk.Tk()
+windows.title("AforroApp")
+windows.iconbitmap("python_icon.ico")
 
-text = tk.StringVar()
+# Create a menu bar
+menubar = tk.Menu()
+windows.config(menu=menubar)
 
-# Add a label welcome
-label = tk.Label(root, text="You're using Tkinter! Let's practise!")
-label.pack()
+# Create entries menu
+menu = tk.Menu(menubar, tearoff=0)
+menu_2 = tk.Menu(menubar, tearoff=0)
 
-# Label for name
-label_name = tk.Label(root, text="Name: ")
-label_name.pack(padx=1, pady=1)
+# Menu open and load data Excel file and insert new data
+menubar.add_cascade(label="File", menu=menu)
+menu.add_command(label="Open", command=load_excel_data)
+menu.add_command(label="Insert new data", command=save_excel_info)
 
-# Create a Text widget
-text_widget = tk.Text(root, height=1, width=15)
-text_widget.pack()
+# Menu close menu window
+menubar.add_cascade(label="Windows", menu=menu_2)
+menu_2.add_command(label="Exit", command=windows.quit)
 
-# Create a button to trigger the function
-button = tk.Button(root, text="Print", command=print_text)
-button.pack()
-
-# Create a button to load the Excel file
-load_button = tk.Button(root, text="Load Excel File", command=load_excel_data)
-load_button.pack()
-
-# Run the main loop
-root.mainloop()
+windows.mainloop()
